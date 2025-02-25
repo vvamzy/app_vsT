1c447c66627ada97075e5bbb526c8978ac0f0ba8
vvamzy <86004108+vvamzy@users.noreply.github.com>
2025-02-25 23:00:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, Trivy Scanner!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
